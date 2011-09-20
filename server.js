7dbb91f26785b06b368758d70f5ace0b1b3775a4
Seth Hayward <seth.hayward@gmail.com>
2011-09-20 00:57:06 -0400

    Sending a param back.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-//These are required node library files to assist us.
 var sys = require('sys'),
    http = require('http');
 
-//http is the http library
-//so on every request the given function will be called with a request and a response argument. 
 http.createServer(function (req, res) {
-  //write out the response headers
   res.writeHead(200, {'Content-Type': 'text/html'});
-  //finish it up with what you want to send to the client
-  res.end("Hello!");
-}).listen(80); //Tell it to listen on port 8000
+
+  //Add a listener to the request for the request body data
+  var thing = null;
+  req.addListener('data', function(data){
+    thing = data.toString();
+    }).addListener('end', function() {
+      res.end("Post data:"+thing);
+    });  
+
+}).listen(80);
