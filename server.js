d1e34a2718217527bbfd9f904e10750c1adbeb68
Seth Hayward <seth.hayward@gmail.com>
2011-09-20 00:39:28 -0400

    Modified hello world.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("http").createServer(function (req, res) {
   res.writeHead(200, {})
-  res.end("Hello, world!")
+  res.end("Hello, sad world!")
 }).listen(80)
 console.log("waiting to say hello.")
