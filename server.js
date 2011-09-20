417c06ac8a84bab8be5c263947f09f4c48d1c68e
Seth Hayward <seth.hayward@gmail.com>
2011-09-20 00:15:19 -0400

    NO ideas.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require("http").createServer(function (req, res) {
   res.writeHead(200, {})
   res.end("Hello, world!")
-}).listen(80)
-console.log("waiting to say hello.")
 
 var foobar = require("./foobar"),
        sys = require("sys");
 
 sys.puts("Foobar: " + foobar.bar("bar"));
+
+
+}).listen(80)
+console.log("waiting to say hello.")
