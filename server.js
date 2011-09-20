f147204e8043418a28e8a6450056c5bf85fe770c
Seth Hayward <seth.hayward@gmail.com>
2011-09-20 00:14:07 -0400

    Modules.

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ require("http").createServer(function (req, res) {
   res.end("Hello, world!")
 }).listen(80)
 console.log("waiting to say hello.")
+
+var foobar = require("./foobar"),
+       sys = require("sys");
+
+sys.puts("Foobar: " + foobar.bar("bar"));
