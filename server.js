3fe947eeaf7efcd4cdbabf401c1ebbf594abf546
Seth Hayward <seth.hayward@gmail.com>
2011-09-19 22:39:32 -0400

    Back to hello.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var foobar = require("./foobar"),
-       sys = require("sys");
-
-sys.puts("Foobar: " + foobar.bar("bar"));
+require("http").createServer(function (req, res) {
+  res.writeHead(200, {})
+  res.end("Hello, world!")
+}).listen(80)
+console.log("waiting to say hello.")
