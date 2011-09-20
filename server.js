f08c9657aaae1582fe15add27e50fe1d1694007d
Seth Hayward <seth.hayward@gmail.com>
2011-09-20 00:41:47 -0400

    Trying new tutorial.

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-require("http").createServer(function (req, res) {
-  res.writeHead(200, {})
-  res.end("Hello, sad world!")
-}).listen(80)
-console.log("waiting to say hello.")
+//These are required node library files to assist us.
+var sys = require('sys'),
+   http = require('http');
+
+//http is the http library
+//so on every request the given function will be called with a request and a response argument. 
+http.createServer(function (req, res) {
+  //write out the response headers
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  //finish it up with what you want to send to the client
+  res.end("Hello!");
+}).listen(8000); //Tell it to listen on port 8000
